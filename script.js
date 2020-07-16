f3c3fe305fb2febeea751b041f5c105b1d662b4a
Alexandre Leclerc <alexandre.leclerc63@gmail.com>
2020-07-16 09:19:37 +0200

    16/07/2020

--- script.js
+++ script.js
@@ -1,49 +1,106 @@
-let i = 0;
-let play = true;
-const container = document.getElementById("slider");
-const slides = container.querySelectorAll(".slider-items");
-
-function refresh() {
-    for (slide of slides) {
-        slide.className = "slider-items display-none";
+/*Diapo*/
+class Diapo {
+    constructor(container) {
+        this.i = 0;
+        this.play = true;
+        this.container = document.getElementById(container);
+        this.slides = this.container.querySelectorAll(".slider-items");
+        this.container.querySelector(".next").addEventListener("click", () => { this.next() });
+        this.container.querySelector(".prev").addEventListener("click", () => { this.prev() });
+        this.btn = this.container.querySelector(".btn");
+        this.btn.addEventListener("click", () => { this.playPause() });
+        this.refresh();
+        this.changeImage = setInterval(() => { this.next() }, 1000);
+        this.keys();
     };
 
-    slides[i].className = "slider-items display-flex";
-};
-
-function next() {
-    i++;
-
-    if (i >= slides.length) i = 0;
-
-    refresh();
-};
-
-function prev() {
-    i--;
-
-    if (i < 0) i = slides.length - 1;
-
-    refresh();
-};
-
-container.querySelector(".next").addEventListener("click", next);
-container.querySelector(".prev").addEventListener("click", prev);
-const btn = container.querySelector(".btn");
-
-btn.addEventListener("click", playPause);
-
-refresh();
-
-let changeImage = setInterval(next, 500);
-
-function playPause() {
-    if(play){
-        clearInterval(changeImage);
-        btn.className = "btn play";
-    } else {
-        changeImage = setInterval(next, 500);
-        btn.className = "btn pause";
+    keys() {
+        document.addEventListener("keydown", (e) => {
+            switch (e.key) {
+                case "ArrowLeft": this.prev(); break;
+                case "ArrowRight": this.next(); break;
+            }
+        });
     };
-    play = !play;
+
+    refresh() {
+        for (let slide of this.slides) {
+            slide.className = "slider-items display-none";
+        };
+
+        this.slides[this.i].className = "slider-items display-flex";
+    };
+
+    next() {
+        this.i++;
+
+        if (this.i >= this.slides.length) this.i = 0;
+
+        this.refresh();
+    };
+
+    prev() {
+        this.i--;
+
+        if (this.i < 0) this.i = this.slides.length - 1;
+
+        this.refresh();
+    };
+
+    playPause() {
+        if (this.play) {
+            clearInterval(this.changeImage);
+            this.btn.className = "btn play";
+        } else {
+            this.changeImage = setInterval(() => { this.next() }, 1000);
+            this.btn.className = "btn pause";
+        };
+        this.play = !this.play;
+    };
+};
+
+const diapo = new Diapo("slider");
+
+/*Map*/
+
+let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'sk.eyJ1Ijoic2lsdmVyYWxleHR2IiwiYSI6ImNrYzM2eXdnOTI0bmsyeW80emVvMGd5aHIifQ.JPNW0aI1E6eDH6K90Pvt9w'
+}).addTo(mymap);
+
+let marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+const contractName = 'Lyon';
+
+const apiKey = '95095e2132411b499e70e8bd5b06f24d7e925d41';
+
+const url = `https://api.jcdecaux.com/vls/v1/stations?contract=${contractName}&apiKey=${apiKey}`;
+
+function ajaxGet(url, callback) {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+            callback(response);
+        }
+    };
+    request.open("GET", url);
+    request.send();
+};
+
+
+console.log(url);
+
+ajaxGet(url, (response) => {
+    console.log(response);
+});
+
+for each(let item in ajaxGet(url, (response))){
+    console.log(item);
 };
