cbf964ed064f17579fb2eebe75ea73bca81ba9b0
Alexandre <alexandre.leclerc63@gmail.com>
2020-08-26 13:09:05 +0200

    Ajout canvas commenté

--- script.js
+++ script.js
@@ -126,7 +126,7 @@ ajaxGet(url, (response) => {
     };
 });
 
-var canvas = document.getElementById('signature');
+/*var canvas = document.getElementById('signature');
 var ctx = canvas.getContext('2d');
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, 300, 200);
@@ -137,4 +137,32 @@ ctx.stroke();
 
 let mouseCoords = addEventListener(ctx, 'mousemove',);
 console.log(mouseCoords);
-/*Comment*/
+
+var signaturePad = new SignaturePad(document.getElementById('signature'), {
+    backgroundColor: 'rgba(255, 255, 255, 0)',
+    penColor: 'rgb(0, 0, 0)'
+});
+
+var signature = {
+    canvas: null,
+    clearButton: null,
+    resizeCanvas: function resizeCanvas() {
+        // When zoomed out to less than 100%, for some very strange reason,
+        // some browsers report devicePixelRatio as less than 1
+        // and only part of the canvas is cleared then.
+        var ratio = Math.max(window.devicePixelRatio || 1, 1);
+        this.canvas.width = canvas.offsetWidth * ratio;
+        this.canvas.height = canvas.offsetHeight * ratio;
+        this.canvas.getContext("2d").scale(ratio, ratio);
+    },
+    init: function init() {
+        this.canvas = document.querySelector(".signature-pad");
+        this.clearButton = document.getElementById('clear');
+        signaturePad = new SignaturePad(this.canvas);
+        this.clearButton.addEventListener('click', function (event) {
+            signaturePad.clear();
+        });
+    }
+};
+
+signature.init();*/
