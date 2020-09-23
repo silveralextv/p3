6ceb337682178eacd5e30c620328ecd7fccc5aea
Alexandre Leclerc <alexandre.leclerc63@gmail.com>
2020-09-23 22:31:23 +0200

    23/09/2020 22:30

--- book.js
+++ book.js
@@ -1,8 +1,9 @@
 function initBook() {
-    let bookHour;
-
     document.querySelector("#book").addEventListener("click", () => {
-        bookHour = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+        const end = new Date();
+        end.setMinutes(end.getMinutes + 20);
+        console.log(end);
+        let bookHour = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
         localStorage.setItem("bookHour", bookHour);
         let lastName = document.getElementById("lastname").value;
         localStorage.setItem("lastName", lastName);
