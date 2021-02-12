505ec8a0c67897cbf415e1d518c7aa4f1026ee79
Alexandre Leclerc <alexandre.leclerc63@gmail.com>
2021-02-12 15:02:23 +0100

    Timer update

--- book.js
+++ book.js
@@ -2,7 +2,7 @@ class Book{
     constructor(){
         this.book = document.querySelector("#book")
         this.book.addEventListener("click", () =>{
-            this.end = Math.floor(Date.now() / 1000) + 20 * 60
+            this.end = Math.floor(Date.now() / 1000) + 20
             sessionStorage.setItem('end', this.end)
             new Timer("#timer")
         })
@@ -16,8 +16,20 @@ class Timer{
         if (this.end){
             this.now = Math.floor(Date.now() / 1000)
             if (this.end > this.now) {
-                setInterval( () => {
+                const interval = setInterval( () => {
                     this.total = this.end - this.now
+                    if (this.total < 0){
+                        window.clearInterval(interval)
+                        return
+                    }
                     this.minutes = Math.floor(this.total / 60)
                     this.secondes = ("0" + (this.total % 60)).substr(-2)
-        
+                    this.container.innerHTML =
+                    `<p>Votre vélo doit être récupérer dans les ` + this.minutes +  ` : ` + this.secondes + ` restantes</p>`
+                    console.log("fait")
+                    this.now = Math.floor(Date.now() / 1000)
+                }, 1000)
+            }
+        }
+    }
+}
